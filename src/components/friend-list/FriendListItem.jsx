1cb085ea95006d6003cf4f6ec1a1bacca99c0a69
Hanna Tubolets <anna.tubolets@gmail.com>
2023-02-08 00:51:02 +0300

    Added friends styles

--- src/components/friend-list/FriendListItem.jsx
+++ src/components/friend-list/FriendListItem.jsx
@@ -4,7 +4,7 @@ import css from './Friends.module.css';
 export const FriendListItem = ({avatar, name, isOnline}) => {
     return (
         <li className={css.item}>
-            <span className={css.status}></span>
+            <span className={isOnline ? css.online : css.offline}></span>
             <img
                 className={css.avatar}
                 src={avatar}
